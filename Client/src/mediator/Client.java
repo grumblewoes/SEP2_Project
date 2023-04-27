2f83c7ea31c2bb5730bfb04a975d8f0af6864242
gramovi4a <331957@viauc.dk>
2023-04-27 21:21:54 +0200

    Implemented Server and Client

--- Client/src/mediator/Client.java
+++ Client/src/mediator/Client.java
@@ -1,66 +1,120 @@
 package mediator;
 
-
 import modelClient.Model;
 
 import java.rmi.Naming;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 
-public class Client implements Model, Remote
+public class Client implements Model
 {
 
-	private RemoteModel server;
+  private RemoteModel server;
 
+  public Client()
+  {
+    try
+    {
+      start();
+      server = (RemoteModel) Naming.lookup(
+          "rmi://localhost:1099/ValhallaServer");
+      //remoteModel.addListener(this);
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+    }
+  }
 
-	public Client()
-	{
-		try
-		{
-			start();
-			server = (RemoteModel) Naming.lookup("rmi://localhost:1099/ValhallaServer");
-			//remoteModel.addListener(this);
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
+  public void start()
+  {
+    try
+    {
+      UnicastRemoteObject.exportObject(this, 0);
+    }
+    catch (Exception e)
+    {
+      System.out.println(e.getMessage());
+    }
+  }
 
-	public void start() {
-		try {
-			UnicastRemoteObject.exportObject(this, 0);
-		}
-		catch (Exception e)
-		{
-			System.out.println(e.getMessage());
-		}
-	}
+  public boolean createUser(String firstName, String lastName, String userName,
+      String password, int height, int weight)
+  {
+    try
+    {
+      return server.createUser(firstName, lastName, userName, password, height,
+          weight);
+    }
+    catch (RemoteException e)
+    {
+      throw new RuntimeException(e);
+    }
+  }
 
+  public boolean login(String username, String password)
+  {
+    try
+    {
+      return server.login(username, password);
+    }
+    catch (RemoteException e)
+    {
+      throw new RuntimeException(e);
+    }
+  }
 
-	public boolean createUser(String firstName, String lastName, String userName, String password, int height, int weight) {
-		try
-		{
-			return server.createUser(firstName, lastName, userName, password, height, weight);
-		}
-		catch (RemoteException e)
-		{
-			throw new RuntimeException(e);
-		}
-	}
+  public boolean createFolder(String username, String name)
+  {
+    try
+    {
+      return server.createFolder(username, name);
+    }
+    catch (RemoteException e)
+    {
+      throw new RuntimeException(e);
+    }
+  }
 
+  public boolean removeFolder(String username, String name)
+  {
+    try
+    {
+      return server.removeFolder(username, name);
+    }
+    catch (RemoteException e)
+    {
+      throw new RuntimeException(e);
+    }
+  }
 
-	public boolean login(String username, String password){
-		try
-		{
-			return server.login(username, password);
-		}
-		catch (RemoteException e)
-		{
-			throw new RuntimeException(e);
-		}
-	}
+  public boolean editFolder(String username, String oldName, String newName)
+  {
+    try
+    {
+      return server.editFolder(username, oldName, newName);
+    }
+    catch (RemoteException e)
+    {
+      throw new RuntimeException(e);
+    }
+
+  }
+
+  public ArrayList<String> getFolderList(String username)
+  {
+    try
+    {
+      return server.getFolderList(username);
+    }
+    catch (RemoteException e)
+    {
+      throw new RuntimeException(e);
+    }
+
+  }
 
 }
 
--- Server/src/mediator/RemoteModel.java
+++ Server/src/mediator/RemoteModel.java
@@ -2,6 +2,7 @@ package mediator;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 public interface RemoteModel extends Remote
 {
@@ -9,5 +10,9 @@ public interface RemoteModel extends Remote
 	boolean createUser(String firstName, String lastName, String userName, String password, int height, int weight) throws
 			RemoteException;
 	boolean login(String username, String password) throws RemoteException;
+	boolean createFolder(String username, String name)  throws RemoteException;;
+	boolean removeFolder(String username, String name)  throws RemoteException;;
+	boolean editFolder(String username, String oldName, String newName)  throws RemoteException;;
+	ArrayList<String> getFolderList(String username)  throws RemoteException;;
 
 }
