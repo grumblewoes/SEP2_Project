a3c30f612a007d243f4be579a25c9cf4752aa982
Damian Trafiałek <damian.trafialek@gmail.com>
2023-04-30 12:28:21 +0200

    add: viewModel implementation

--- Client/src/viewModel/ViewModelFactory.java
+++ Client/src/viewModel/ViewModelFactory.java
@@ -6,10 +6,25 @@ public class ViewModelFactory {
 
 	private CreateUserViewModel createUserViewModel;
 	private LoginViewModel loginViewModel;
+	private FolderViewModel folderViewModel;
+	private AddExerciseViewModel addExerciseViewModel;
+
+	private ViewState viewState;
 
 	public ViewModelFactory(Model model) {
+		viewState = new ViewState();
 		this.createUserViewModel=new CreateUserViewModel(model);
 		this.loginViewModel = new LoginViewModel(model);
+		this.folderViewModel=new FolderViewModel(model,viewState);
+		this.addExerciseViewModel = new AddExerciseViewModel(model,viewState);
+	}
+
+	public FolderViewModel getFolderViewModel() {
+		return folderViewModel;
+	}
+
+	public AddExerciseViewModel getAddExerciseViewModel() {
+		return addExerciseViewModel;
 	}
 
 	public CreateUserViewModel getCreateUserViewModel() {
