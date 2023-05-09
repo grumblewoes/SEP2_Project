804e32bccf943148a61ca8774997af45f09dfa00
grumblewoes <culturuse@gmail.com>
2023-05-09 17:25:43 +0200

    Task 13 view and viewmodel wip

--- Client/src/view/ManageCoachViewController.java
+++ Client/src/view/ManageCoachViewController.java
@@ -1,5 +1,8 @@
 package view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import viewModel.ManageCoachViewModel;
 import viewModel.ViewModel;
 import javafx.scene.layout.Region;
@@ -8,16 +11,37 @@ public class ManageCoachViewController extends ViewController{
     private ViewHandler viewHandler;
     private ManageCoachViewModel viewModel;
 
+    @FXML
+    private TextField coachField;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private Label headerLabel;
+
     @Override
     public void init(ViewHandler viewHandler, ViewModel viewModel, Region root) {
         this.viewHandler = viewHandler;
         this.viewModel = (ManageCoachViewModel)viewModel;
         this.root = root;
+
+        coachField.textProperty().bindBidirectional(this.viewModel.getCoachProperty());
     }
 
     @Override
     public void reset() {
         viewModel.clear();
+    }
+
+    @FXML
+    void cancel() {
+        viewHandler.openView("profile");
+    }
+
+    @FXML
+    void submit() {
+        //if success, go back. if fail, stay
     }
 
     private void requestCoach() {
--- Client/src/viewModel/ManageCoachViewModel.java
+++ Client/src/viewModel/ManageCoachViewModel.java
@@ -1,14 +1,18 @@
 package viewModel;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import modelClient.Model;
 
 public class ManageCoachViewModel extends ViewModel {
     private Model model;
     private ViewState viewState;
+    private StringProperty coachProperty;
 
     public ManageCoachViewModel(Model model, ViewState viewState) {
         this.model = model;
         this.viewState = viewState;
+        coachProperty = new SimpleStringProperty();
     }
     @Override
     public void clear() {
@@ -16,7 +20,7 @@ public class ManageCoachViewModel extends ViewModel {
     }
 
     public void requestCoach() {
-        if (model.requestCoach(viewState.getUsername(), coachField.get()))
+        if (model.requestCoach(viewState.getUsername(), coachProperty.get()))
         {
             errorLabel.set("Pending");
             //set label to approved or sum
@@ -25,5 +29,9 @@ public class ManageCoachViewModel extends ViewModel {
         else
         {}
             //set label to error, clear text field
+    }
+
+    public StringProperty getCoachProperty() {
+        return coachProperty;
     }
 }
