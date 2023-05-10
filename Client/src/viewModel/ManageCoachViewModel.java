ec058483853ce36a76dfeda3bac2d8abce3725ae
grumblewoes <culturuse@gmail.com>
2023-05-10 08:22:34 +0200

    Task 13 viewmodel wip

--- Client/src/viewModel/ManageCoachViewModel.java
+++ Client/src/viewModel/ManageCoachViewModel.java
@@ -7,28 +7,38 @@ import modelClient.Model;
 public class ManageCoachViewModel extends ViewModel {
     private Model model;
     private ViewState viewState;
-    private StringProperty coachProperty;
+    private StringProperty coachProperty, errorProperty;
 
     public ManageCoachViewModel(Model model, ViewState viewState) {
         this.model = model;
         this.viewState = viewState;
         coachProperty = new SimpleStringProperty();
+        errorProperty = new SimpleStringProperty();
     }
     @Override
     public void clear() {
-
+        //check to see if user is coach, or if they have a coach already
+        errorProperty.set("");
+        if (model.getCoach(viewState.getUsername()) != null)
+        {
+            //if coach is viewing, should I remove the button from their profile entirely?
+            //where to put logic for hiding/showing buttons
+            //where in sep1 is the hiding buttons logic
+            //request_list? or just have a trigger that does something
+        }
     }
 
     public void requestCoach() {
+        //if the request goes through
         if (model.requestCoach(viewState.getUsername(), coachProperty.get()))
         {
-            errorLabel.set("Pending");
-            //set label to approved or sum
+            errorProperty.set("Pending");
         }
-
         else
-        {}
-            //set label to error, clear text field
+        {
+            errorProperty.set("An error occurred during request transaction.");
+            coachProperty.set("");
+        }
     }
 
     public StringProperty getCoachProperty() {
