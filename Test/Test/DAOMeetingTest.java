69fdbfc70847fcdf00c8237ecdcada2640796caf
grumblewoes <culturuse@gmail.com>
2023-05-16 11:53:23 +0200

    test skeletons

--- Test/Test/DAOMeetingTest.java
+++ Test/Test/DAOMeetingTest.java
@@ -1,362 +1,356 @@
-import modelServer.DAO.implementation.CoachDAO;
-import modelServer.DAO.implementation.UserDAO;
-import modelServer.DAO.interfaces.ICoachDAO;
-import modelServer.DAO.interfaces.IUserDAO;
+import modelServer.DAO.implementation.MeetingDAO;
+import modelServer.DAO.interfaces.IMeetingDAO;
+import modelServer.DbContext.DBConnection;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
 
 class DAOMeetingTest
 {
-    private ICoachDAO cdao;
-    private IUserDAO udao;
+  IMeetingDAO mdao;
+  String trainee, coach;
+  LocalDate date1, date2, date3;
+  PreparedStatement statement;
+  DBConnection db;
+  Connection connection;
 
-    @BeforeEach
-    void setUp() {
-        cdao = new CoachDAO();
-        udao = new UserDAO();
-    }
+  @BeforeEach void setUp() throws SQLException
+  {
+    mdao = new MeetingDAO();
+    trainee = "d";
+    coach = "coach";
+    date1 = LocalDate.of(2023, 5, 16);
+    date2 = LocalDate.of(2023, 5, 17);
+    date3 = LocalDate.of(2023, 5, 18);
 
-    @Test
-    void addCoachZero() {
-        assertThrows(SQLException.class, ()->cdao.addCoach(null, null, null, null, 0, 0, 0, 0, 0, null, false));
+    try
+    {
+      db = DBConnection.getInstance();
+      connection = db.getConnection();
+      statement = connection.prepareStatement("delete from meeting_list;"
+          + "delete from meeting_request;");
+      statement.executeUpdate();
     }
-    @Test
-    void addCoachOne() {
-        assertTrue(()-> {
-            try {
-                return cdao.addCoach("TheLegend27", "LegendThe27", "Alpha", "Male", 180, 80, 220, 220, 220, "On that grind", true);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        });
+    catch (SQLException e)
+    {
+      throw new RuntimeException(e);
     }
-    @Test
-    void addCoachMany() {
-        assertTrue(()-> {
-            try {
-                return cdao.addCoach("TheLegend28", "g", "Alpha1", "Male1", 180, 80, 220, 220, 220, "On that grind", true);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        });
-        assertTrue(()-> {
-            try {
-                return cdao.addCoach("TheLegend29", "g", "Alpha2", "Male2", 180, 80, 220, 220, 220, "On that grind", true);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        });
-        assertTrue(()-> {
-            try {
-                return cdao.addCoach("TheLegend30", "g", "Alpha3", "Male3", 180, 80, 220, 220, 220, "On that grind", true);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        });
+    finally {
+      connection.close();
     }
-    @Test
-    void addCoachBoundary() {
-        //already tested
-    }
-    @Test
-    void addCoachException() {
-        //inserting same coach twice (i.e coach w same username. are we making check constraints for first/last name?)
-        try {
-            cdao.addCoach("TheLegend42", "g", "Alpha1", "Male1", 180, 80, 220, 220, 220, "On that grind", true);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            assertFalse(cdao.addCoach("TheLegend42", "g", "Alpha1", "Male1", 180, 80, 220, 220, 220, "On that grind", true));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
+  }
 
-    @Test
-    void removeCoachZero() {
-        try {
-            assertFalse(cdao.removeCoach(null));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    @Test
-    void removeCoachOne() {
-        try {
-            cdao.addCoach("TheLegend31", "g", "Alpha", "Male", 180, 80, 220, 220, 220, "On that grind", true);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        assertTrue(()-> {
-            try {
-                return cdao.removeCoach("TheLegend31");
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        });
-    }
-    @Test
-    void removeCoachMany() {
-        try {
-            cdao.addCoach("TheLegend32", "g", "Alpha", "Male", 180, 80, 220, 220, 220, "On that grind", true);
-            cdao.addCoach("TheLegend33", "g", "Alpha", "Male", 180, 80, 220, 220, 220, "On that grind", true);
-            cdao.addCoach("TheLegend34", "g", "Alpha", "Male", 180, 80, 220, 220, 220, "On that grind", true);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        assertTrue(()-> {
-            try {
-                return cdao.removeCoach("TheLegend32");
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        });
-        assertTrue(()-> {
-            try {
-                return cdao.removeCoach("TheLegend33");
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        });
-        assertTrue(()-> {
-            try {
-                return cdao.removeCoach("TheLegend34");
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        });
-    }
-    @Test
-    void removeCoachBoundary() {
-        try {
-            cdao.addCoach("TheLegend32", "32LegendThe", "Alpha", "Male", 180, 80, 220, 220, 220, "On that grind", true);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            assertTrue(cdao.removeCoach("TheLegend32"));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            assertFalse(cdao.removeCoach("TheLegend32"));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    @Test
-    void removeCoachException() {
-        //removing a coach that was never listed
-        try {
-            assertFalse(cdao.removeCoach("TheLegend69"));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
+  @Test void approveMeetingZero()
+  {
+    Assertions.assertThrows(NullPointerException.class, () -> {
+      try {
+        mdao.approveMeeting(null, null, null);
+      } catch (SQLException e) {
+        throw new RuntimeException(e);
+      }
+    });
+  }
+  @Test void approveMeetingOne() throws SQLException
+  {
+    try
+    {
+      db = DBConnection.getInstance();
+      connection = db.getConnection();
+      statement = connection.prepareStatement("insert into meeting_request values('d', 'coach', '2023-05-16');");
+      statement.executeUpdate();
 
-    @Test
-    void getCoachZero() {
-        try {
-            assertNull(cdao.getCoach("noobMan27"));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    @Test
-    void getCoachOne() {
-        assertDoesNotThrow(()-> cdao.getCoach("d"));
-    }
-    @Test
-    void getCoachMany() {
-        assertDoesNotThrow(()-> cdao.getCoach("noobMan27"));
-        assertDoesNotThrow(()-> cdao.getCoach("noobMan28"));
-        assertDoesNotThrow(()-> cdao.getCoach("noobMan29"));
-    }
-    @Test
-    void getCoachBoundary() {
-        // wut
-    }
-    @Test
-    void getCoachException() {
-        //getting a coach that doesn't exist, either cuz user doesn't exist or user has no coach
-        try {
-            assertNull(cdao.getCoach("noobMan69")); //user doesn't exist
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            assertNull(cdao.getCoach("noobMan420")); //user has no coach
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Test
-    void isCoachZero() {
-        try {
-            assertFalse(cdao.isCoach(null));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    @Test
-    void isCoachOne() {
-        try {
-            assertTrue(cdao.isCoach("TheLegend27"));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    @Test
-    void isCoachMany() {
-        try {
-            assertTrue(cdao.isCoach("TheLegend27"));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            assertTrue(cdao.isCoach("TheLegend28"));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            assertTrue(cdao.isCoach("TheLegend29"));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    @Test
-    void isCoachBoundary() {
-        //no boundary
-    }
-    @Test
-    void isCoachException() {
-        //coach that does not exist in the system, aka random or trainee
-        try {
-            assertFalse(cdao.isCoach("coolguy67"));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Test
-    void requestCoachZero() {
-        try {
-            assertFalse(cdao.requestCoach(null, null));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    @Test
-    void requestCoachOne() {
-        try { //change this
-            assertTrue(cdao.requestCoach("d", "TheLegend27"));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    @Test
-    void requestCoachMany() {
-        try { //change these plz
-            assertTrue(cdao.requestCoach("a", "TheLegend28"));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            assertTrue(cdao.requestCoach("j", "TheLegend28"));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            assertTrue(cdao.requestCoach("e", "TheLegend28"));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    @Test
-    void requestCoachBoundary() {
-        //?? no boundary
-    }
-    @Test
-    void requestCoachException() {
-        //user already requested a coach. change this plz
-        try {
-            assertFalse(cdao.requestCoach("d", "TheLegend27"));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Test
-    void removeCoachAssignmentZero() {
+      Assertions.assertTrue(()-> {
         try
         {
-            assertFalse(cdao.removeCoachAssignment(null));
+          return mdao.approveMeeting(trainee, coach, Date.valueOf(date1));
         }
         catch (SQLException e)
         {
-            throw new RuntimeException(e);
+          throw new RuntimeException(e);
         }
+      });
+
+      statement = connection.prepareStatement("delete from meeting_list;");
+      statement.executeUpdate();
     }
-    @Test
-    void removeCoachAssignmentOne() {
+    catch (SQLException e)
+    {
+      throw new RuntimeException(e);
+    }
+    finally {
+      connection.close();
+    }
+
+  }
+  @Test void approveMeetingMany() throws SQLException
+  {
+    try
+    {
+      db = DBConnection.getInstance();
+      connection = db.getConnection();
+      statement = connection.prepareStatement("insert into meeting_request values('d', 'coach', '2023-05-16');"
+          + "insert into meeting_request values('d', 'coach', '2023-05-17');"
+          + "insert into meeting_request values('d', 'coach', '2023-05-18');");
+      statement.executeUpdate();
+
+      Assertions.assertTrue(()-> {
         try
         {
-            assertTrue(cdao.removeCoachAssignment("d"));
+          return mdao.approveMeeting(trainee, coach, date1);
+
         }
         catch (SQLException e)
         {
-            throw new RuntimeException(e);
+          throw new RuntimeException(e);
         }
-    }
-    @Test
-    void removeCoachAssignmentMany() {
+      });
+      Assertions.assertTrue(()-> {
         try
         {
-            assertTrue(cdao.removeCoachAssignment("d"));
+          return mdao.approveMeeting(trainee, coach, date2);
+
         }
         catch (SQLException e)
         {
-            throw new RuntimeException(e);
+          throw new RuntimeException(e);
         }
+      });
+      Assertions.assertTrue(()-> {
         try
         {
-            assertTrue(cdao.removeCoachAssignment("d"));
+          return mdao.approveMeeting(trainee, coach, date3);
+
         }
         catch (SQLException e)
         {
-            throw new RuntimeException(e);
+          throw new RuntimeException(e);
         }
+      });
+    }
+    catch (SQLException e)
+    {
+      throw new RuntimeException(e);
+    }
+    finally {
+      connection.close();
+    }
+  }
+  @Test void approveMeetingBoundary()
+  {
+    //no boundary
+  }
+  @Test void approveMeetingException()
+  {
+    //trainee cannot request from coach that is not assigned to them, so no
+  }
+
+  @Test void denyMeetingZero()
+  {
+    Assertions.assertThrows(NullPointerException.class, () -> {
+      try {
+        mdao.denyMeeting(null, null, null);
+      } catch (SQLException e) {
+        throw new RuntimeException(e);
+      }
+    });
+  }
+  @Test void denyMeetingOne() throws SQLException
+  {
+    try
+    {
+      db = DBConnection.getInstance();
+      connection = db.getConnection();
+      statement = connection.prepareStatement("insert into meeting_request values('d', 'coach', '2023-05-16');");
+      statement.executeUpdate();
+
+      Assertions.assertTrue(()-> {
         try
         {
-            assertTrue(cdao.removeCoachAssignment("d"));
+          return mdao.denyMeeting(trainee, coach,
+              date1);
         }
         catch (SQLException e)
         {
-            throw new RuntimeException(e);
+          throw new RuntimeException(e);
         }
+      });
+
+      statement = connection.prepareStatement("delete from meeting_list;");
+      statement.executeUpdate();
     }
-    @Test
-    void removeCoachAssignmentBoundary() {
-        //wut
+    catch (SQLException e)
+    {
+      throw new RuntimeException(e);
     }
-    @Test
-    void removeCoachAssignmentException() {
-        //for trainee who does not have coach. tune this
-        assertFalse(()-> {
-            try
-            {
-                return cdao.removeCoachAssignment("d");
-            }
-            catch (SQLException e)
-            {
-                throw new RuntimeException(e);
-            }
-        });
+    finally {
+      connection.close();
     }
+  }
+  @Test void denyMeetingMany() throws SQLException
+  {
+    try
+    {
+      db = DBConnection.getInstance();
+      connection = db.getConnection();
+      statement = connection.prepareStatement("insert into meeting_request values('d', 'coach', '2023-05-16');"
+          + "insert into meeting_request values('d', 'coach', '2023-05-17');"
+          + "insert into meeting_request values('d', 'coach', '2023-05-18');");
+      statement.executeUpdate();
+
+      Assertions.assertTrue(()-> {
+        try
+        {
+          return mdao.denyMeeting(trainee, coach,
+              date1);
+        }
+        catch (SQLException e)
+        {
+          throw new RuntimeException(e);
+        }
+      });
+      Assertions.assertTrue(()-> {
+        try
+        {
+          return mdao.denyMeeting(trainee, coach,
+              date2);
+        }
+        catch (SQLException e)
+        {
+          throw new RuntimeException(e);
+        }
+      });
+      Assertions.assertTrue(()-> {
+        try
+        {
+          return mdao.denyMeeting(trainee, coach,
+              date3);
+        }
+        catch (SQLException e)
+        {
+          throw new RuntimeException(e);
+        }
+      });
+
+      statement = connection.prepareStatement("delete from meeting_list;");
+      statement.executeUpdate();
+    }
+    catch (SQLException e)
+    {
+      throw new RuntimeException(e);
+    }
+    finally {
+      connection.close();
+    }
+  }
+  @Test void denyMeetingBoundary()
+  {
+    //no boundary
+  }
+  @Test void denyMeetingException()
+  {
+    //already tested
+  }
+
+  @Test void getTraineeMeetingRequestsZero() {
+    try {
+      Assertions.assertNotNull(mdao.getTraineeMeetingRequests(null));
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+  @Test void getTraineeMeetingRequestsOne() {
+    try {
+      Assertions.assertNotNull(mdao.getTraineeMeetingRequests(coach));
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+  @Test void getTraineeMeetingRequestsMany() {
+    try {
+      Assertions.assertNotNull(mdao.getTraineeMeetingRequests(coach));
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+    try {
+      Assertions.assertNotNull(mdao.getTraineeMeetingRequests(coach));
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+    try {
+      Assertions.assertNotNull(mdao.getTraineeMeetingRequests(coach));
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+  @Test void getTraineeMeetingRequestsBoundary() {
+    //no boundary, no test
+  }
+  @Test void getTraineeMeetingRequestsException() {
+    //already tested
+  }
+
+  @Test void getCoachMeetingsZero() {
+
+    Assertions.assertDoesNotThrow(() -> mdao.getCoachMeetings(null));
+  }
+  @Test void getCoachMeetingsOne() {
+    try {
+      Assertions.assertNotNull(mdao.getCoachMeetings(coach));
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+  @Test void getCoachMeetingsMany() {
+    try {
+      Assertions.assertNotNull(mdao.getCoachMeetings(coach));
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+    try {
+      Assertions.assertNotNull(mdao.getCoachMeetings(coach));
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+    try {
+      Assertions.assertNotNull(mdao.getCoachMeetings(coach));
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+  @Test void getCoachMeetingsBoundary() {
+    //no boundary
+  }
+  @Test void getCoachMeetingsException() {
+    //tested already
+  }
+
+  @Test void getTraineeMeetingRequestZero() {
+    //when merging, might have issues. two methods with similar name
+  }
+  @Test void getTraineeMeetingRequestOne() {
+
+  }
+  @Test void getTraineeMeetingRequestMany() {}
+  @Test void getTraineeMeetingRequestBoundary() {}
+  @Test void getTraineeMeetingRequestException() {}
+
+  //returns boolean
+  @Test void sendMeetingRequestZero() {}
+  @Test void sendMeetingRequestOne() {}
+  @Test void sendMeetingRequestMany() {}
+  @Test void sendMeetingRequestBoundary() {}
+  @Test void sendMeetingRequestException() {}
+
+  //returns boolean
+  @Test void removeMeetingZero() {}
+  @Test void removeMeetingOne() {}
+  @Test void removeMeetingMany() {}
+  @Test void removeMeetingBoundary() {}
+  @Test void removeMeetingException() {}
 }
