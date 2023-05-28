136924b1e897326e20c9da3cd53b45ee9e98aeac
Damian Trafiałek <damian.trafialek@gmail.com>
2023-05-28 15:11:39 +0200

    add: commenbt about viewControllers

--- Client/src/view/ViewHandler.java
+++ Client/src/view/ViewHandler.java
@@ -89,6 +89,30 @@ public class ViewHandler {
 	public void openView(String id) {
 		switch (id)
 		{
+			/***
+			 * Abstract factory - a pattern that creates one superior class (factory) that is abstract and other classes should extend/implement all its methods
+			 * Factory method - a pattern that allows to create similar objects of the same class with some automation
+			 * Factory is producing objects
+			 *
+			 * Why here the abstract factory is not a good solution? Patterns should simplify some actions or make executing them easier or more understandable
+			 * In this case we would have as many factories as controllers so it doesnt make sense - doesnt not simplify anything. Products are of the same type - viewController
+			 *
+			 * Why factory method is not the best idea - products that are being produced are of the same type but they must be connected with unique viewModel.
+			 * each of view models extend viewModel class but they are unique as well. There would have to be a logic somewhere that would assign a correct viewModel to correct viewController.
+			 * So even though the first part would simplify the code the second part makes it impossible to write less code.
+			 *
+			 *
+			 * Why multiton might be a good idea, there all are of the same type so it would simplify as well.
+			 * The issue is with viewModels that are different for each viewController -> there would have to be some logic deciding which one is a correct one
+			 *
+			 * CONCLUSION:
+			 * It is impossible to simplify the code using any of patterns listed above. Since they won't simplify abything there is no use of using them.
+			 *
+			 */
+			case "xxx":
+				loadViewController("xxx");
+				break;
+
 			case "createProfile":
 				createUserViewController = loadViewController("createProfile.fxml", createUserViewController, viewModelFactory.getCreateUserViewModel());
 				break;
@@ -140,7 +164,9 @@ public class ViewHandler {
 		primaryStage.show();
 		primaryStage.getIcons().add(0,new Image("/assets/ValhallaFitness.png"));
 	}
+	private void loadViewController(String fxmlFileName){
 
+	}
 	private ViewController loadViewController(String fxmlFile, ViewController viewController, ViewModel viewModel) {
 		if (viewController == null)
 		{
